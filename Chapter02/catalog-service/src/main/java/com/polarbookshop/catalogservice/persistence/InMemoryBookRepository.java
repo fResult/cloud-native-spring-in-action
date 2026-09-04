ce015208ffd9b565e7f7c8a65b74d017650cb45a
fResult <styxmaz@gmail.com>
2026-09-04 15:57:15 +0700

    refactor(catalog-service): use Vavr immutable map for InMemoryBookRepository

--- Chapter02/catalog-service/src/main/java/com/polarbookshop/catalogservice/persistence/InMemoryBookRepository.java
+++ Chapter02/catalog-service/src/main/java/com/polarbookshop/catalogservice/persistence/InMemoryBookRepository.java
@@ -2,39 +2,41 @@ package com.polarbookshop.catalogservice.persistence;
 
 import com.polarbookshop.catalogservice.domain.Book;
 import com.polarbookshop.catalogservice.domain.BookRepository;
+import io.vavr.collection.HashMap;
 import io.vavr.collection.List;
+import io.vavr.collection.Map;
 import io.vavr.control.Option;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class InMemoryBookRepository implements BookRepository {
-  private static final Map<String, Book> books = new ConcurrentHashMap<>();
+  private final AtomicReference<Map<String, Book>> booksRef =
+      new AtomicReference<>(HashMap.empty());
 
   @Override
   public List<Book> findAll() {
-    return List.ofAll(books.values());
+    return booksRef.get().values().toList();
   }
 
   @Override
   public Option<Book> findByIsbn(String isbn) {
-    return existsByIsbn(isbn) ? Option.of(books.get(isbn)) : Option.none();
+    return booksRef.get().get(isbn);
   }
 
   @Override
   public boolean existsByIsbn(String isbn) {
-    return books.get(isbn) != null;
+    return booksRef.get().containsKey(isbn);
   }
 
   @Override
   public Book save(Book book) {
-    books.put(book.isbn(), book);
+    booksRef.updateAndGet(books -> books.put(book.isbn(), book));
     return book;
   }
 
   @Override
   public void deleteByIsbn(String isbn) {
-    books.remove(isbn);
+    booksRef.updateAndGet(books -> books.remove(isbn));
   }
 }
