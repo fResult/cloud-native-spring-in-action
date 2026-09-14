68f53033b0aa83a49d6e82360ae224005b46278e
fResult <styxmaz@gmail.com>
2026-09-14 23:46:07 +0700

    chore(config-service): configure Spotless formating and reformat code

--- Chapter04/config-service/build.gradle.kts
+++ Chapter04/config-service/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-  java
-  id("org.springframework.boot") version "4.1.1"
-  id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "4.1.1"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.diffplug.spotless") version "8.10.1"
 }
 
 group = "com.polarbookshop"
@@ -9,29 +10,48 @@ version = "0.0.1-SNAPSHOT"
 description = "Centralizes the application configuration"
 
 java {
-  toolchain {
-    languageVersion = JavaLanguageVersion.of(26)
-  }
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(26)
+    }
 }
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 extra["springCloudVersion"] = "2025.1.3"
 
 dependencies {
-  implementation("org.springframework.cloud:spring-cloud-config-server")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.cloud:spring-cloud-config-server")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+spotless {
+    java {
+        palantirJavaFormat()
+            .style("GOOGLE")
+            .formatJavadoc(true)
+
+        importOrder()
+        removeUnusedImports()
+
+        target("**/*.java")
+        targetExclude("**/build/**")
+    }
+
+    kotlinGradle {
+        ktlint()
+        target("*.gradle.kts")
+    }
 }
 
 dependencyManagement {
-  imports {
-    mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-  }
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.withType<Test> {
-  useJUnitPlatform()
+    useJUnitPlatform()
 }
--- Chapter04/config-service/src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
+++ Chapter04/config-service/src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
-	static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
-	}
+  static void main(String[] args) {
+    SpringApplication.run(ConfigServiceApplication.class, args);
+  }
 }
