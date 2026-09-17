0133edd60c47759268512d8a3b394fb03a0632a7
fResult <styxmaz@gmail.com>
2026-09-17 19:07:42 +0700

    feat(catalog-service): initialize PostgreSQL schema from `schema.sql` for `Book` table

---
+++ Chapter05/catalog-service/src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+  id BIGSERIAL PRIMARY KEY NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  isbn VARCHAR(20) UNIQUE NOT NULL,
+  price NUMERIC(10, 2) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  created_date TIMESTAMP NOT NULL,
+  last_modified_date TIMESTAMP NOT NULL,
+  "version" INTEGER NOT NULL
+);
