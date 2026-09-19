1472424663c5ed76a7512ba03dd53143ab23c6fd
fResult <styxmaz@gmail.com>
2026-09-20 01:59:38 +0700

    test(catalog-service): assert `deleteByIsbn` removes persisted book by id lookup

--- Chapter05/catalog-service/src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTest.java
+++ Chapter05/catalog-service/src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTest.java
@@ -3,6 +3,7 @@ package com.polarbookshop.catalogservice.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.polarbookshop.catalogservice.common.config.DataConfiguration;
@@ -115,7 +116,8 @@ class BookRepositoryJdbcTest {
     bookRepository.deleteByIsbn(bookIsbn);
 
     // Then
-    val maybeBook = bookRepository.findByIsbn(bookIsbn);
+    assertNotNull(persistedBook.id());
+    val maybeBook = bookRepository.findBookById(persistedBook.id());
     assertTrue(maybeBook.isEmpty());
   }
 }
