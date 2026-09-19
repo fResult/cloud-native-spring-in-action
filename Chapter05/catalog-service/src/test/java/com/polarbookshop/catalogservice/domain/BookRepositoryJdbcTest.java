3c9ea5edeed6435af9cace6385e32d00bf19280c
fResult <styxmaz@gmail.com>
2026-09-20 01:38:02 +0700

    test(catalog-service): add JDBC repository `findAll` test and refine PostgreSQL Testcontainer setup

--- Chapter05/catalog-service/src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTest.java
+++ Chapter05/catalog-service/src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTest.java
@@ -1,10 +1,13 @@
 package com.polarbookshop.catalogservice.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.polarbookshop.catalogservice.common.config.DataConfiguration;
+import io.vavr.collection.List;
 import java.math.BigDecimal;
+import java.util.function.Predicate;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @DataJdbcTest
 @Testcontainers
@@ -22,13 +26,33 @@ import org.testcontainers.postgresql.PostgreSQLContainer;
 class BookRepositoryJdbcTest {
   @Container
   @ServiceConnection
-  static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:18-alpine");
+  static final PostgreSQLContainer postgres =
+      new PostgreSQLContainer(DockerImageName.parse("postgres:18-alpine"));
 
   @Autowired
   private BookRepository bookRepository;
 
   @Autowired
   private JdbcAggregateTemplate jdbcAggregateTemplate;
+
+  @Test
+  void findAllBooks() {
+    // Given
+    val book1 = Book.of("1234561235", "Title", "Author", BigDecimal.valueOf(12.90));
+    val book2 = Book.of("1234561236", "Another Title", "Author", BigDecimal.valueOf(12.90));
+    jdbcAggregateTemplate.insertAll(List.of(book1, book2));
+
+    // When
+    val actualBooks = bookRepository.findAll();
+    //    val actualBookIsbns = actualBooks.map(Book::isbn);
+
+    // Then
+    assertThat(actualBooks.filter(isBook1OrBook2(book1.isbn(), book2.isbn()))).hasSize(2);
+  }
+
+  private Predicate<Book> isBook1OrBook2(String isbn1, String isbn2) {
+    return book -> book.isbn().equals(isbn1) || book.isbn().equals(isbn2);
+  }
 
   @Test
   void findBookByIsbnWhenExisting() {
